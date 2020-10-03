2898a787a2bd14096bf157e5e1eef1eb4150109b
thanghaqn2000 <57361234+thanghaqn2000@users.noreply.github.com>
2020-10-03 21:46:40 +0700

    first commit

---
+++ app/src/main/java/com/example/login_hoducthang_1811505310338/Custom_Listview.java
@@ -0,0 +1,12 @@
+package com.example.login_hoducthang_1811505310338;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class Custom_Listview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom__listview);
+        getSupportActionBar().setTitle("Custom ListView_HoDucThang_1811505310338");
+    }
+}
